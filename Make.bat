dc5eade4cfe54062a53b593ec884029d9ea32101
TheDeadFish <michaelstamper1@gmail.com>
2020-04-24 00:47:16 +0000

    use native toolchains for both x86 and x64, -m32 is sketchy

--- Make.bat
+++ Make.bat
@@ -1,15 +1,21 @@
-setlocal
-call egcc64.bat 
 set SRC=hotpatch.cc mempatch.cc xheap.cc apihook.cc callpatch.cc
 set OBJ=hotpatch.o mempatch.o xheap.o apihook.o callpatch.o
 
-gcc %SRC% %CCFLAGS2% -m32 -c 
-ar rcs %PROGRAMS%\local\lib32\libexshit.a %OBJ%
-gcc %SRC% %CCFLAGS2% -c
-ar rcs %PROGRAMS%\local\lib64\libexshit.a %OBJ%
+:: build
+call :build 32
+call :build 64
 del *.o
 
 :: copy include files
 copy /Y hotpatch.h %PROGRAMS%\local\include
 copy /Y apihook.h %PROGRAMS%\local\include
 copy /Y callpatch.h %PROGRAMS%\local\include
+exit /b
+
+:build
+	setlocal
+	call egccx.bat x%1
+	gcc %SRC% %CCFLAGS2% -c
+	ar rcs %PROGRAMS%\local\lib%1\libexshit.a %OBJ%
+	endlocal
+	exit /b
